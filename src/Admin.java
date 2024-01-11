795a05a9a4ef5c883e0bbb4260360cf7cb9cc227
Miez13 <hakimimuhammad777@gmail.com>
2024-01-11 14:33:52 +0800

    UPDATE METHOD

--- src/Admin.java
+++ src/Admin.java
@@ -1,3 +1,9 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
 public class Admin {
     private int adminID;
     private String password;
@@ -131,4 +137,275 @@ public class Admin {
         System.out.println("Product does not exist!");
         return false;
     }
+    
+    String filePath = "data/product.txt";
+    FileProduct fileProduct = new FileProduct(filePath);
+
+    public void updateProductName(LinkedList products) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter product name to update : ");
+        String productName = sc.nextLine();
+        System.out.println("Enter new product name : ");
+        String newProductName = sc.nextLine();
+    
+        Object data = products.getFirst();
+        boolean updated = false;
+    
+        while (data != null) {
+            Product product = (Product) data;
+            if (product.getProductName().equalsIgnoreCase(productName)) {
+                product.setProductName(newProductName);
+                break;  // Assuming product names are unique, exit loop once found
+            }
+            data = products.getNext();
+        }
+    
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+            data = products.getFirst();  // Reset the pointer
+            while (data != null) {
+                String entry = ((Product) data).toString();
+                writer.println(entry);
+                data = products.getNext();
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing to user file : " + e.getMessage());
+        }
+
+        if (updated) {
+            String[] items = {"\nU","P","D","A","T","I","N","G",".",".",".\n"};
+
+            for (String item : items) 
+            {
+                System.out.print(item);
+
+                try {
+                    Thread.sleep(150);
+                }
+                catch (InterruptedException e) 
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        else {
+            System.out.println("Product name not found or no update performed.");
+        }
+        sc.close();
+    }
+
+    public void updateProductID(LinkedList products) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter product ID to update : ");
+        String productID = sc.nextLine();
+        System.out.print("Enter new product ID : ");
+        String newProductID = sc.nextLine();
+    
+        Object data = products.getFirst();
+    
+        boolean updated = false;
+
+        while (data != null) {
+            Product product = (Product) data;
+            if (product.getProductID().equalsIgnoreCase(productID)) {
+                product.setProductID(newProductID);
+                break;  // Assuming product names are unique, exit loop once found
+            }
+            data = products.getNext();
+        }
+    
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+            data = products.getFirst();  // Reset the pointer
+            while (data != null) {
+                String entry = ((Product) data).toString();
+                writer.println(entry);
+                data = products.getNext();
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing to user file : " + e.getMessage());
+        }
+
+        if (updated) {
+            String[] items = {"\nU","P","D","A","T","I","N","G",".",".",".\n"};
+
+            for (String item : items) 
+            {
+                System.out.print(item);
+
+                try {
+                    Thread.sleep(150);
+                }
+                catch (InterruptedException e) 
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        else {
+            System.out.println("Product name not found or no update performed.");
+        }
+        sc.close();
+    }
+
+    public void updateProductBrand(LinkedList products) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter product ID to update: ");
+        String productID = sc.nextLine();
+        System.out.println("Enter new product brand : ");
+        String newProductBrand = sc.nextLine();
+    
+        Object data = products.getFirst();
+    
+        boolean updated = false;
+
+        while (data != null) {
+            Product product = (Product) data;
+            if (product.getProductID().equalsIgnoreCase(productID)) {
+                product.setBrand(newProductBrand);
+                break;  // Assuming product names are unique, exit loop once found
+            }
+            data = products.getNext();
+        }
+    
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+            data = products.getFirst();  // Reset the pointer
+            while (data != null) {
+                String entry = ((Product) data).toString();
+                writer.println(entry);
+                data = products.getNext();
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing to user file : " + e.getMessage());
+        }
+
+        if (updated) {
+            String[] items = {"\nU","P","D","A","T","I","N","G",".",".",".\n"};
+
+            for (String item : items) 
+            {
+                System.out.print(item);
+
+                try {
+                    Thread.sleep(150);
+                }
+                catch (InterruptedException e) 
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        else {
+            System.out.println("Product name not found or no update performed.");
+        }
+        sc.close();
+    }
+
+    public void updateProductPrice(LinkedList products) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter product ID to update : ");
+        String productID = sc.nextLine();
+        System.out.println("Enter new product price : ");
+        double newProductPrice = sc.nextDouble();
+    
+        Object data = products.getFirst();
+    
+        boolean updated = false;
+        while (data != null) {
+            Product product = (Product) data;
+            if (product.getProductID().equalsIgnoreCase(productID)) {
+                product.setProductPrice(newProductPrice);
+                break;  // Assuming product names are unique, exit loop once found
+            }
+            data = products.getNext();
+        }
+    
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+            data = products.getFirst();  // Reset the pointer
+            while (data != null) {
+                String entry = ((Product) data).toString();
+                writer.println(entry);
+                data = products.getNext();
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing to user file : " + e.getMessage());
+        }
+
+        if (updated) {
+            String[] items = {"\nU","P","D","A","T","I","N","G",".",".",".\n"};
+
+            for (String item : items) 
+            {
+                System.out.print(item);
+
+                try {
+                    Thread.sleep(150);
+                }
+                catch (InterruptedException e) 
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        else {
+            System.out.println("Product name not found or no update performed.");
+        }
+        sc.close();
+    }
+    
+    public void updateProductAvailability(LinkedList products) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter product ID to update: ");
+        String productID = sc.nextLine();
+        System.out.println("Enter new product availability: ");
+        String newProductAvailability = sc.nextLine();
+    
+        Object data = products.getFirst();
+    
+        boolean updated = false;
+        while (data != null) {
+            Product product = (Product) data;
+            if (product.getProductID().equalsIgnoreCase(productID)) {
+                product.setProductAvailability(newProductAvailability);
+                break;  // Assuming product names are unique, exit loop once found
+            }
+            data = products.getNext();
+        }
+    
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+            data = products.getFirst();  // Reset the pointer
+            while (data != null) {
+                String entry = ((Product) data).toString();
+                writer.println(entry);
+                data = products.getNext();
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing to user file : " + e.getMessage());
+        }
+
+        if (updated) {
+            String[] items = {"\nU","P","D","A","T","I","N","G",".",".",".\n"};
+
+            for (String item : items) 
+            {
+                System.out.print(item);
+
+                try {
+                    Thread.sleep(150);
+                }
+                catch (InterruptedException e) 
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        else {
+            System.out.println("Product name not found or no update performed.");
+        }
+        sc.close();
+    }
+    
 }
